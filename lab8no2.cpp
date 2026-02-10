fb70a12c9ad098b42c8d53e9200f5d1240501b66
KT <s6804062612021@email.kmutnb.ac.th>
2026-02-10 20:44:53 +0700

    show no. of hardest no.

--- lab8no2.cpp
+++ lab8no2.cpp
@@ -2,6 +2,7 @@
 
 int checkscore(char std[]);
 int no1stat(char first[][10]);
+int hardestno(char std[][10]);
 
 int main() {
     int i;
@@ -15,8 +16,10 @@ int main() {
 	{'B','B','A','C','C','D','E','E','A','D'},//7
 	{'E','B','E','C','C','D','E','E','A','D'}};//7
 
-	for(i=0;i<8;i++) printf("std %d => %d\n", (i+1), checkscore(ans[i]));
-	printf("There are %d students who answered question 1 correctly",no1stat(ans));
+	for(i=0;i<8;i++)
+	printf("std %d => %d\n", (i+1), checkscore(ans[i]));		
+	printf("There are %d students who answered question 1 correctly.\n",no1stat(ans));
+	printf("The hardest question is question %d.\n",hardestno(ans));
 }
 
 
@@ -35,4 +38,18 @@ int no1stat(char first[][10]){
 		if(first[i][0]=='D') num = num+1;
 	}
 	return num;
+}
+
+int hardestno(char std[][10]){
+	char charkeys[10]={'D','B','D','C','C','D','A','E','A','D'};
+	int count[10]={0,0,0,0,0,0,0,0,0,0},i,j,indexmin=0;
+	for(i=0;i<10;i++){
+		for(j=0;j<8;j++){
+			if(std[i][j]==charkeys[j]) count[j] = count[j]+1;
+		}
+	}
+	for(i=1;i<8;i++){
+		if(count[i]<count[indexmin]) indexmin = i;
+	}
+	return indexmin+1;
 }
